5b33c200bcf76e759831cd1e72acaf7eab0a64d5
Student-prohladnoy-life <108605032+Student-prohladnoy-life@users.noreply.github.com>
2022-08-03 18:54:38 +0300

    add new step_2 functional and instruction

--- step_1.py
+++ step_1.py
@@ -4,4 +4,8 @@
 # 4. commit -  для сохранения истории изменений
 # 5.  push - заливаем изменения на github
 # 6. pull reguest - сравнить новую и старую ветку,с дальнейшим слиянием
+#7. отправка  https://github.com/Student-prohladnoy-life/git-github-pycarm/pull/1
+# 8. проверка дз
+# 9. merge pull reguest
 print('step_1')
+print('step_2')
